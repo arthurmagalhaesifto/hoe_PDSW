447ab5f4c9100cb5ddc9e372a65beb1ea8628638
Arthur da Silva Magalhães <104932165+arthurmagalhaesifto@users.noreply.github.com>
2023-05-04 21:53:24 -0300

    Teste com JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function rodar(){
-    var semente1 = document.getElementById("semente1").innerHTML;
+    var semente1 = document.getElementsByTagName("h3").innerHTML;
     document.getElementById("selecionado").innerHTML = "Você selecionou: " + semente1;
 }
