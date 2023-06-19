9f51e9ba068e664a2f8fcafdef0a71669db874c5
nanico525 <104927277+nanico525@users.noreply.github.com>
2023-06-19 20:56:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask
 from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import time
 
 app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+control_pins = [14,15,18,23]
+
+for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+
+halfstep_seq = [
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
+]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cakes')
-def cakes():
-    return render_template('cakes.html')
-
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('page.html', name=name)
+@app.route('/run_motor')
+def run_motor():
+    for i in range(512):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            time.sleep(0.001)
+    return 'Motor running'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
+
